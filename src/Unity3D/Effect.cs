a1fa86af6eb091dc07937af1e700f4e3370d2c85
Andrew <therealandrewjoy@gmail.com>
2015-04-23 12:45:42 +1000

    Removed Unity3D timed effect destruction

--- src/Unity3D/Effect.cs
+++ src/Unity3D/Effect.cs
@@ -9,22 +9,11 @@ namespace Slerpy.Unity3D
     {
         private float timeRunning = 0.0f;
 
-        [SerializeField]
-        private float timeRemaining = 0.0f;
-
         public float TimeRunning
         {
             get
             {
                 return this.timeRunning;
-            }
-        }
-
-        public float TimeRemaining
-        {
-            get
-            {
-                return this.timeRemaining;
             }
         }
 
@@ -37,16 +26,9 @@ namespace Slerpy.Unity3D
 
         protected void Update()
         {
-            if (this.timeRemaining > 0.0f && (this.timeRemaining -= Time.deltaTime) < 0.0f)
-            {
-                MonoBehaviour.Destroy(this);
-            }
-            else
-            {
-                this.timeRunning += Time.deltaTime;
+            this.timeRunning += Time.deltaTime;
 
-                this.ProcessEffect(Time.deltaTime);
-            }
+            this.ProcessEffect(Time.deltaTime);
         }
     }
 }
