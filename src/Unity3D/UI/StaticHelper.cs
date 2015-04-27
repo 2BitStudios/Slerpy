3dce73196b32d612e3265cf569fdd6648869909f
Andrew <therealandrewjoy@gmail.com>
2015-04-27 16:17:08 +1000

    Added Unity3D static method wrapper for UI use

---
+++ src/Unity3D/UI/StaticHelper.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Slerpy.Unity3D.UI
+{
+    public sealed class StaticHelper : MonoBehaviour
+    {
+        public void LoadLevel(string levelName)
+        {
+            Application.LoadLevel(levelName);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        public void Destroy(GameObject target)
+        {
+            GameObject.Destroy(target);
+        }
+
+        public void Destroy(Component target)
+        {
+            Component.Destroy(target);
+        }
+
+        public void Instantiate(GameObject prefab)
+        {
+            GameObject.Instantiate(prefab);
+        }
+
+        public void Instantiate(GameObject prefab, Vector3 position)
+        {
+            GameObject.Instantiate(prefab, position, Quaternion.identity);
+        }
+
+        public void Instantiate(GameObject prefab, Vector3 position, Quaternion rotation)
+        {
+            GameObject.Instantiate(prefab, position, rotation);
+        }
+    }
+}
