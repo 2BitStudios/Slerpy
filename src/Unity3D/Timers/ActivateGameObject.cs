b4f974b131c27ee3dd00378a6b081f3d3ccbcf03
Andrew <therealandrewjoy@gmail.com>
2015-05-19 11:51:51 +1000

    Added Unity3D game object activation timers

---
+++ src/Unity3D/Timers/ActivateGameObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Slerpy.Unity3D.Timers
+{
+    public sealed class ActivateGameObject : Timer<GameObject>
+    {
+        protected override void Trigger()
+        {
+            this.Target.SetActive(true);
+        }
+    }
+}
---
+++ src/Unity3D/Timers/DeactivateGameObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Slerpy.Unity3D.Timers
+{
+    public sealed class DeactivateGameObject : Timer<GameObject>
+    {
+        protected override void Trigger()
+        {
+            this.Target.SetActive(false);
+        }
+    }
+}
