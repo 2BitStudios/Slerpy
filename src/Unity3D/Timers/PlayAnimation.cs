8aefc9370095611a4f009d8e947980d864a46f60
Andrew <therealandrewjoy@gmail.com>
2015-06-02 13:48:16 +1000

    Added Unity3D Play/Stop animation timers

---
+++ src/Unity3D/Timers/PlayAnimation.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Slerpy.Unity3D.Timers
+{
+    public sealed class PlayAnimation : Timer<Animation>
+    {
+        [SerializeField]
+        private string animationName = null;
+
+        [SerializeField]
+        private bool shouldCrossfade = true;
+
+        public string AnimationName
+        {
+            get
+            {
+                return this.animationName;
+            }
+
+            set
+            {
+                this.animationName = value;
+            }
+        }
+
+        public bool ShouldCrossfade
+        {
+            get
+            {
+                return this.shouldCrossfade;
+            }
+
+            set
+            {
+                this.shouldCrossfade = value;
+            }
+        }
+
+        protected override void Trigger()
+        {
+            if (this.shouldCrossfade)
+            {
+                this.Target.CrossFade(this.animationName);
+            }
+            else
+            {
+                this.Target.Play(this.animationName);
+            }
+        }
+    }
+}
---
+++ src/Unity3D/Timers/StopAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Slerpy.Unity3D.Timers
+{
+    public sealed class StopAnimation : Timer<Animation>
+    {
+        protected override void Trigger()
+        {
+            this.Target.Stop();
+        }
+    }
+}
