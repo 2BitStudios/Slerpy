5adecdc791b64eadb6add31b0e15bc6d535b22e1
Andrew <therealandrewjoy@gmail.com>
2015-12-01 15:08:56 +1000

    Added Unity3D.Editor project

---
+++ src/Unity3D.Editor/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Slerpy.Unity3D.Editor")]
+[assembly: AssemblyDescription("Editor for Slerpy Unity3D wrapper")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2cdb8705-fd1d-4137-9e8b-79fb3ac40e38")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
