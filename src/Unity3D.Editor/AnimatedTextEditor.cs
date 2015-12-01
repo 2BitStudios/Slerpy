72541d1c5a601e5f5cfed5777c779e9118d0927f
Andrew <therealandrewjoy@gmail.com>
2015-12-01 15:09:18 +1000

    Added Unity3D AnimatedText custom editor

---
+++ src/Unity3D.Editor/AnimatedTextEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+using Slerpy.Unity3D;
+
+[CustomEditor(typeof(AnimatedText))]
+[CanEditMultipleObjects]
+public sealed class AnimatedTextEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
